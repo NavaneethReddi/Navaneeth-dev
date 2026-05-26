4b29fb10c7aa9053c583a5ed5cf11688f7fef0b7
NAVANEETH REDDY PINNAPUREDDY <34224971+NavaneethReddi@users.noreply.github.com>
2026-05-25 22:24:51 -0500

    feat: SEO - app/robots.ts

--- app/robots.ts
+++ app/robots.ts
@@ -1,14 +1,9 @@
-import type { MetadataRoute } from 'next'
-import { siteConfig } from '@/lib/site'
+import { MetadataRoute } from 'next'
 
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: {
-      userAgent: '*',
-      allow: '/',
-      disallow: ['/api/', '/__clerk/'],
-    },
-    sitemap: `${siteConfig.url}/sitemap.xml`,
-    host: siteConfig.url,
+    rules: [{ userAgent: '*', allow: '/' }],
+    sitemap: 'https://clerk-nextjs-phi.vercel.app/sitemap.xml',
+    host: 'https://clerk-nextjs-phi.vercel.app',
   }
 }
