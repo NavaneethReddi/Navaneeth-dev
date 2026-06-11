fbe2178bbb36057cd43ea409f76e940051e0cdd5
Navaneeth Reddy <reddynavaneeth6@gmail.com>
2026-06-11 06:56:12 -0500

    Fix: Update number of developers mentored in AboutSection

--- app/about/AboutSection.tsx
+++ app/about/AboutSection.tsx
@@ -2,7 +2,7 @@ const highlights = [
   { value: "12+", label: "Years Experience" },
   { value: "30+", label: "Enterprise Apps" },
   { value: "40%", label: "Perf Improvement" },
-  { value: "20+", label: "Devs Mentored" },
+  { value: "50+", label: "Devs Mentored" },
 ];
 
 export default function AboutSection() {
